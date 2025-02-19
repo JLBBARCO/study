3f6a03ea1e8ab7844d8a64059cc086210fd2f741
JLBBARCO <159829462+JLBBARCO@users.noreply.github.com>
2025-02-19 18:17:59 -0300

    19/02/2025

--- ColAdolpho/back-end/atividades/atividade01/01.js
+++ ColAdolpho/back-end/atividades/atividade01/01.js
@@ -1,3 +1,5 @@
 const nome = 'José Luiz';
 let idade = 16, cidade = 'Astorga';
 console.log(nome, idade, cidade);
+const mensagem = 'Meu nome é ' + nome + ' e eu moro em ' + cidade;
+console.log(mensagem)
---
+++ ColAdolpho/back-end/atividades/atividade01/03.js
@@ -0,0 +1,3 @@
+let nascimento = 2008;
+let idade = 2025 - nascimento;
+console.log(idade);
