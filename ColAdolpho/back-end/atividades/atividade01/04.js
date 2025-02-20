d402fa65afbc5ac89e1a1f3ebf9107efc55a0e96
Jose Luiz Bruiani Barco <jose.barco@escola.pr.gov.br>
2025-02-20 10:59:42 -0300

    20/02/2025

--- ColAdolpho/back-end/atividades/atividade01/04.js
+++ ColAdolpho/back-end/atividades/atividade01/04.js
@@ -2,4 +2,4 @@ let a = 5, b = 10;
 let c = a;
 a = b;
 b = c;
-console.log('Valor de a: ' + a + '\nValor de b: ' + b)
+console.log('Valor de a: ' + a + '\nValor de b: ' + b);
---
+++ ColAdolpho/back-end/atividades/atividade01/05.js
@@ -0,0 +1,6 @@
+let n1 = 8, n2 = 3;
+let soma = n1 + n2;
+let subtração = n1 - n2;
+let multiplicação = n1 * n2;
+let divisão = n1 / n2;
+console.log('Resultado: \nSoma:', soma, '\nSubtração:', subtração, '\nMultiplicação:', multiplicação, '\nDivisão:', divisão);
