0b6e904cde73966ef6ada3faa57b3f4b0454f236
Jose Luiz Bruiani Barco <159829462+JLBBARCO@users.noreply.github.com>
2025-03-03 21:38:24 -0300

    03/03/2025

--- js/script.js
+++ js/script.js
@@ -3,19 +3,3 @@ var data = new Date()
 var atual = data.getFullYear()
 var ano = document.querySelector('div#ano')
 ano.innerHTML = `${atual}`
-
-// Thead fixo no topo da tela
-const table = document.querySelector('table');
-    const thead = table.querySelector('thead');
-    const offsetTop = table.offsetTop;
-    const theadHeight = thead.offsetHeight;
-
-    window.addEventListener('scroll', () => {
-        const scrollPosition = window.scrollY;
-
-        if (scrollPosition > offsetTop + table.offsetHeight - theadHeight) {
-        thead.classList.add('detached');
-    } else {
-        thead.classList.remove('detached');
-    }
-    });
