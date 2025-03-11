1cc788ec9b2da9b95ba01268c7a6f3d4726dd828
Jose Luiz Bruiani Barco <159829462+JLBBARCO@users.noreply.github.com>
2025-03-11 18:26:07 -0300

    11/03/2025

--- CursoemVideo/python/desafio/070.py
+++ CursoemVideo/python/desafio/070.py
@@ -2,12 +2,9 @@ print('-'*50)
 print('{:^50}'.format('LOJA SUPER BARATÃO'))
 print('-'*50)
 total = c_produto = 0
-produto_barato = ''
-produto = input('Nome do produto: ').strip()
-preço = float(input('Preço: R$'))
-continuar = input('Quer continuar? [S/N] ').strip().upper()[0]
+produto_barato = produto = preço = continuar = ' '
 preço_barato = preço
-while continuar == 'S':
+while True:
     produto = input('Nome do produto: ').strip()
     preço = float(input('Preço: R$'))
     continuar = input('Quer continuar? [S/N] ').strip().upper()[0]
@@ -17,6 +14,8 @@ while continuar == 'S':
     if preço < preço_barato:
         produto_barato = produto
         preço_barato = preço
+    if continuar == 'N':
+        break
 print('{:-^50}'.format(' FIM DO PROGRAMA '))
 print(f'O total da compra foi R${total:.2f}')
 print(f'Temos {c_produto} produtos custando mais de R$1000.00')
