fd21c01b72963889a7c1c008c6b0797c743e68f1
Jose Luiz Bruiani Barco <159829462+JLBBARCO@users.noreply.github.com>
2025-03-20 17:32:56 -0300

    20/03/2025

--- CursoemVideo/python/desafio/072.py
+++ CursoemVideo/python/desafio/072.py
@@ -1,6 +1,15 @@
 n = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
 extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
-user = int(input('Digite um número entre 0 e 20: '))
-while user < 0 or user > 20:
-    user = int(input('Tente novamente. Digite um número entre 0 e 20: '))
-print(f'Você digitou o número {extenso[user]}')
+while True:
+    user = int(input('Digite um número entre 0 e 20: '))
+    continuar = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if 0 <= user <= 20:
+        if continuar == 'S':
+            print(f'Você digitou o número {extenso[user]}')
+        elif continuar == 'N':
+            print(f'Você digitou o número {extenso[user]}')
+            break
+    else:
+        print('Tente novamente.', end=' ')
+        if continuar == 'N':
+            break
--- CursoemVideo/python/desafio/074.py
+++ CursoemVideo/python/desafio/074.py
@@ -1,6 +1,8 @@
 from random import randint
 n = tuple(randint(1, 10) for c in range(5))
-print(f'Os valores sorteados foram: {n}')
+print(f'Os valores sorteados foram: ', end='')
+for number in n:
+    print(f'{number}', end=' ')
 organizado = sorted(n)
-print(f'O maior valor sorteado foi {organizado[-1]}')
+print(f'\nO maior valor sorteado foi {organizado[-1]}')
 print(f'O menor valor sorteado foi {organizado[0]}')
