294e454892742d5463a041366710e92e0943fa0b
Jose Luiz Bruiani Barco <159829462+JLBBARCO@users.noreply.github.com>
2025-04-04 17:56:07 -0300

    04/04/2025

---
+++ cursoemvideo/python/desafio/084.py
@@ -0,0 +1,30 @@
+temp = list()
+principal = list()
+maior = menor = 0
+while True:
+    temp.append(str(input('Nome: ')))
+    temp.append(float(input('Peso: ')))
+    if len(principal) == 0:
+        maior = menor = temp[1]
+    else:
+        if temp[1] > maior:
+            maior = temp[1]
+        if temp[1] < menor:
+            menor = temp[1]
+    principal.append(temp[:])
+    temp.clear()
+    resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print('-=' * 30)
+print(f'Ao todo, você cadastrou {len(principal)} pessoas.')
+print(f'O maior peso foi de {maior}kg. Peso de ', end='')
+for p in principal:
+    if p[1] == maior:
+        print(f'[{p[0]}] ', end='')
+print()
+print(f'O menor peso foi de {menor}kg. Peso de ', end='')
+for p in principal:
+    if p[1] == menor:
+        print(f'[{p[0]}] ', end='')
+print()
--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
-/* Data */
+// Acessibilidade
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('button#botao_acessibilidade').addEventListener('click', function() {
+        document.querySelector('div#opcoes_acessibilidade').style.display = 'block';
+    });
+
+    let fonte = 1;
+    document.querySelector('button#aumentar_fonte').addEventListener('click', function() {
+        fonte += 0.1;
+        document.body.style.fontSize = `${fonte}rem`;
+    });
+
+    document.querySelector('button#diminuir_fonte').addEventListener('click', function() {
+        fonte -= 0.1;
+        document.body.style.fontSize = `${fonte}rem`;
+    });
+
+    document.querySelector('button#fechar_acessibilidade').addEventListener('click', function() {
+        document.querySelector('div#opcoes_acessibilidade').style.display = 'none';
+    });
+
+});
+
+// Data
 var data = new Date()
 var atual = data.getFullYear()
 var ano = document.querySelector('div#ano')
