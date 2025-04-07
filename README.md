d70ab4deb8502a5aa5b2e5b9de5e61b4aedf7fd5
Jose Luiz Bruiani Barco <159829462+JLBBARCO@users.noreply.github.com>
2025-04-07 18:21:22 -0300

    07/04/2025.3

--- README.md
+++ README.md
@@ -11,7 +11,6 @@ Links of pages:
             * [Markdown](https://jlbbarco.github.io/study/cursoemvideo/git-github/markdown/markdown.html)
         * [JavaScript](https://jlbbarco.github.io/study/cursoemvideo/javascript/javascript.html)
         * [Python](https://jlbbarco.github.io/study/cursoemvideo/python/python.html)
-        * [Algorithm](https://jlbbarco.github.io/study/cursoemvideo/algoritmo/algoritmo.html)
     * [College Adolpho](https://jlbbarco.github.io/study/colegio_adolpho/colegio_adolpho.html)
         * [Front-end Programming](https://jlbbarco.github.io/study/colegio_adolpho/programacao_front-end/programacao_front-end.html)
         * [Developer Systems Programing](https://jlbbarco.github.io/study/colegio_adolpho/colegio_adolpho.html)
--- colegio_adolpho/programacao_desenvolvimento_sistemas/atividades/atividade04/ex04.py
+++ colegio_adolpho/programacao_desenvolvimento_sistemas/atividades/atividade04/ex04.py
@@ -8,52 +8,48 @@ from random import randint
 while True:
     pontuação = tentativa_máxima = 10
     tentativas = 0
-    numero_pc = randint(1, 1)
-    pontuação_user = [0, 0]
+    numero_pc = randint(1, 100)
+    pontuação_recorde = 0
     recorde = list()
 
     print('{:=^30}'.format(' JOGO DE ADIVINHAÇÃO '))
     print(f'Tente adivinhar o número entre 1 e 100. Você tem {tentativa_máxima} tentativas!')
 
-    while True:
+    while tentativas < tentativa_máxima:
         tentativas += 1
+        pontuação -= 1
+
+        print('-'*20)
         user = int(input('Digite um número de 1 a 100: '))
-        
-        if user == numero_pc:
-            pontuação + 1
-            print('Você acertou!', end=' ')
 
-            user_name = input('Digite seu nome: ')
-
-            print(f'Seu nome é {user_name}, e usou {tentativas} tentativas. Sua pontuação foi {pontuação} pontos!')
-            
-            pontuação_user[0], pontuação_user[1] = user_name, pontuação
-            recorde.append(pontuação_user[:])
-            pontuação_user.clear()
-
-            break
-
-        elif user < numero_pc:
-
+        if user < numero_pc:
             print('O número é maior!')
 
         elif user > numero_pc:
-
             print('O número é menor!')
-            
-        if tentativas == tentativa_máxima:
 
-            print('Você perdeu!')
-            print(f'O número era {numero_pc}!')
+        elif user == numero_pc:
+            print('Você acertou!')
+            pontuação += 1
+
+
+            if pontuação > pontuação_recorde:
+                user_name = input('Digite seu nome: ')
+                print('Novo recorde!')
+                pontuação_recorde = pontuação
 
             break
 
-        pontuação = tentativas
+        if tentativas == tentativa_máxima:
+            print('Você atingiu o limite de tentativas!', end=' ')
+            print(f'O número era {numero_pc}!')
+            break
 
     continuar = input('Deseja continuar jogando? (S/N): ').strip().upper()[0]
 
     if continuar == 'N':
-        
+        print('{:=^30}'.format(' RECORDE '))
+        print(f'{user_name}: {pontuação_recorde} pontos')
+        print('=' * 30)
         print('Obrigado por jogar!')
-
         break
