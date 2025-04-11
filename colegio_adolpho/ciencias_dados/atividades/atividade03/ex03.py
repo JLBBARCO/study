ef5c13df806870562abbf8ec46089e96440f2738
Jose Luiz Bruiani Barco <jose.barco@escola.pr.gov.br>
2025-04-11 09:26:02 -0300

    10/04/2025

--- colegio_adolpho/ciencias_dados/atividades/atividade03/ex03.py
+++ colegio_adolpho/ciencias_dados/atividades/atividade03/ex03.py
@@ -1,7 +1,7 @@
 import pandas as pd
 weapons = pd.read_csv('skyrim_weapons.csv')
 weapons['Speed'] 
-Skyrim = weapons.drop_duplicates().dropna().drop('Type', axis=1).drop('Speed', axis=1).drop('Perk', axis=1).drop('Upgrade', axis=1).drop('Gold', axis=1).sort_values('Name')
+Skyrim = weapons.drop_duplicates().drop('Type', axis=1).drop('Speed', axis=1).drop('Perk', axis=1).drop('Upgrade', axis=1).drop('Gold', axis=1).sort_values('Category')
 print(Skyrim)
 new_csv = input('Quer salvar como um novo CSV? (S/N): ').upper()[0]
 if new_csv == 'S':
