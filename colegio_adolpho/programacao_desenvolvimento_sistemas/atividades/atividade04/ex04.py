4989f92697c52b29e7870a0d29cdc283ff379f1a
Jose Luiz Bruiani Barco <jose.barco@escola.pr.gov.br>
2025-04-14 11:03:36 -0300

    14/04/2025.2

--- colegio_adolpho/programacao_desenvolvimento_sistemas/atividades/atividade04/ex04.py
+++ colegio_adolpho/programacao_desenvolvimento_sistemas/atividades/atividade04/ex04.py
@@ -16,51 +16,60 @@ while True:
     numero_pc = randint(1, máximo_número)
     pontuação_recorde = 0
 
-    print('{:=^30}'.format(' JOGO DE ADIVINHAÇÃO '))
+    print('{:=^65}'.format(' JOGO DE ADIVINHAÇÃO '))
     print(f'Tente adivinhar o número entre 1 e {máximo_número}. Você tem {tentativa_máxima} tentativas!')
 
     while tentativas < tentativa_máxima:
         tentativas += 1
         pontuação -= 1
 
-        print('-'*20)
-        user = int(input('Digite um número de 1 a 100: '))
-
-        if user >= máximo_número:
-            print(f'Número inválido! Digite um número entre 1 e {máximo_número}')
-
-        elif user < numero_pc:
-            print('O número é maior!')
-
-        elif user > numero_pc:
-            print('O número é menor!')
-
-        elif user == numero_pc:
-            print('Você acertou!')
-            pontuação += 1
-
-            user_name = input('Digite seu nome: ')
-            pontuação_recorde = pontuação
-            recorde.append(user_name)
-            recorde.append(pontuação)
-
-            rank.append(recorde[:])
-
-            if pontuação > pontuação_recorde:
-                print('Novo recorde!')
-
-            break
+        print('-'*65)
+        user = int(input(f'Digite um número de 1 a {máximo_número}: '))
 
         if tentativas == tentativa_máxima:
             print('Você atingiu o limite de tentativas!', end=' ')
             print(f'O número era {numero_pc}!')
+            pontuação = 0
             break
+        else:
+            if user > máximo_número:
+                print(f'Número inválido! Digite um número entre 1 e {máximo_número}')
+
+            elif user < numero_pc:
+                print('O número é maior!')
+
+            elif user > numero_pc:
+                print('O número é menor!')
+
+            elif not user:
+                print('Digite um número válido!')
+
+            elif user == numero_pc:
+                print('Você acertou!')
+                pontuação += 1
+
+                user_name = input('Digite seu nome: ')
+                pontuação_recorde = pontuação
+                recorde.append(pontuação)
+                recorde.append(user_name)
+
+                rank.append(recorde[:])
+
+                if pontuação > pontuação_recorde:
+                    print('Novo recorde!')
+
+                break
+
 
     continuar = input('Deseja continuar jogando? (S/N): ').strip().upper()[0]
 
     if continuar == 'N':
-        print('{:=^30}'.format(' RECORDE '))
-        print(f'{user_name}: {pontuação_recorde} pontos')
-        print('=' * 30)
+        rank.sort()
+        print('{:=^65}'.format(' RANKING '))
+        print(f'{'NOME':<30}|{'PONTUAÇÃO':>30}')
+        print('='*65)
+        for p, u in rank:
+            print(f'{u:<30}|{p:>30}')
+        print('=' * 65)
         print('Obrigado por jogar!')
         break
