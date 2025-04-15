629a801dfd9f34f1fa9fcd7d72d793782b88270a
Jose Luiz Bruiani Barco <159829462+JLBBARCO@users.noreply.github.com>
2025-04-15 17:13:12 -0300

    15/04/2025.2

--- cursoemvideo/python/aulas/aula021.py
+++ cursoemvideo/python/aulas/aula021.py
@@ -43,3 +43,57 @@
 # print(f'No programa principal, n vale {n}')
 # teste()
 # print(f'No programa principal, x vale {x}')
+
+
+
+# def função():
+#     n1 = 4
+#     print(f'N1 dentro vale {n1}')
+
+# n1 = 2
+# função()
+# print(f'N1 global vale {n1}')
+
+
+# Retorno de Valores
+
+# def somar(a=0, b=0, c=0):
+#     s=a+b+c
+#     return s
+
+# r1 = somar(3,2,5)
+# r2 = somar(2,2)
+# r3 = somar(6)
+
+# print(f'Os resultados foram {r1}, {r2} e {r3}')
+
+
+
+
+# Prática
+
+# def fatorial(num = 1):
+#     f = 1
+#     for c in range(num, 0, -1):
+#         f *= c
+#     return f
+
+# # n = int(input('Digite um número: '))
+# # print(f'O fatorial de {n} é igual a {fatorial(n)}')
+
+# f1 = fatorial(5)
+# f2 = fatorial(4)
+# f3 = fatorial()
+# print(f'Os resultados são {f1}, {f2} e {f3}')
+
+def ParOuImpar(n=0):
+    if n%2==0:
+        return True
+    else:
+        return False
+
+num = int(input('Digite um número: '))
+if ParOuImpar(num) :
+    print('É par!')
+else:
+    print('Não é par!')
