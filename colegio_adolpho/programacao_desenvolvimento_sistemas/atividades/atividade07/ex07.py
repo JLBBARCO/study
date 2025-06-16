f9afad361f37c6bb891fe6b7b95125f67f3c32bc
Jose Luiz Bruiani Barco <jose.barco@escola.pr.gov.br>
2025-06-16 09:04:38 -0300

    16/06/2025

---
+++ colegio_adolpho/programacao_desenvolvimento_sistemas/atividades/atividade07/ex07.py
@@ -0,0 +1,12 @@
+'''
+Programa desenvolvido na disciplina de Programação e Desenvolvimento de Sistemas
+
+Exercício 01 - 2° Trimestre
+
+Desenvolvido por José Luiz B Barco
+'''
+
+números = [1,1,1,1,2,2,2,3,3,3,]
+
+for n in range(0, -11, -1):
+    print(n, end=' ')
