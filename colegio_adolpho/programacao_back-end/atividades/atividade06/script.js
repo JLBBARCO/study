16388e66f2971fd1f3c752dcfa0d2aaefe02e5aa
Jose Luiz Bruiani Barco <jose.barco@escola.pr.gov.br>
2025-07-01 09:34:23 -0300

    01/07/2025

--- colegio_adolpho/programacao_back-end/atividades/atividade06/script.js
+++ colegio_adolpho/programacao_back-end/atividades/atividade06/script.js
@@ -1,3 +1,26 @@
+function resize() {
+  const itens = document.querySelector(".links");
+  const burgerIcon = document.querySelector(".burger-icon");
+  if (!itens || !burgerIcon) return;
+  if (window.innerWidth >= janelaPadrao) {
+    itens.style.display = "contents";
+    burgerIcon.src = `${caminhoRelativoAteRaiz}assets/svg/menu.svg`;
+  } else {
+    itens.style.display = "none";
+    burgerIcon.src = `${caminhoRelativoAteRaiz}assets/svg/menu.svg`;
+  }
+}
+
+function menuToggle() {
+  const menu = document.querySelector(".links");
+
+  if (menu.style.display === "none" || menu.style.display === "") {
+    menu.style.display = "flex";
+  } else {
+    menu.style.display = "none";
+  }
+}
+
 function acessibilidade() {
   const acessibilidade = document.querySelector("div.acessibilidade-opcoes");
 
