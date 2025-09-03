70b338d46894a77b517025bfa4d8006ccf83ceb9
Jose Luiz Bruiani Barco <jose.barco@escola.pr.gov.br>
2025-09-03 12:40:54 -0300

    03/09/2025

--- colegio_adolpho/3_serie/programacao_desenvolvimento_sistemas/atividades/atividade13/ex13.py
+++ colegio_adolpho/3_serie/programacao_desenvolvimento_sistemas/atividades/atividade13/ex13.py
@@ -14,11 +14,19 @@ class App (ctk.CTk):
         self.frame2 = ctk.CTkScrollableFrame(self, width=200, height=200)
         self.frame2.grid(row=1, column=1, padx=20, pady=20)
 
-        self.button = ctk.CTkButton(self.frame1, text='Um Botão', command=self.button_click)
-        self.button.grid(row=1, column=0)
+        self.button = ctk.CTkButton(self, text='Um Botão', command=self.button_click)
+        self.button.grid(row=2, column=0)
+
+        self.entrada = ctk.CTkEntry(self, placeholder_text='Digite seu nome: ')
+        self.entrada.grid(row=1, column=0)
 
     def button_click(self):
-        print('O botão foi pressionado')
+        dados = self.entrada.get()
+
+        diálogo = ctk.CTkInputDialog(text='Digite sua idade: ', title='Janela de entrada')
+        print(f'Sua idade é: {diálogo.get_input()}')
+
+        self.texto.configure(text=dados)
 
 app = App()
 app.mainloop()
