abe1babdac610fbd0d49208f93f4f1643fcb5cde
Jose Luiz Bruiani Barco <159829462+JLBBARCO@users.noreply.github.com>
2025-09-05 06:20:40 -0300

    05/09/2025

--- colegio_adolpho/3_serie/programacao_back-end/atividades/atividade09/script.js
+++ colegio_adolpho/3_serie/programacao_back-end/atividades/atividade09/script.js
@@ -1,16 +1,13 @@
 const praiaTabatinga = document.getElementById("praia-tabatinga");
-const peninsulaValdes = document.getElementById("peninsula-valdes");
 
 praiaTabatinga.addEventListener("mouseover", mouseHover);
 praiaTabatinga.addEventListener("mouseout", mouseOut);
-peninsulaValdes.addEventListener("mouseover", mouseHover);
-peninsulaValdes.addEventListener("mouseout", mouseOut);
 
 function mouseHover() {
-  const info = document.querySelector(".info"); // corrigido para classe
+  const info = document.querySelector(".info");
   info.style.display = "block";
 }
 function mouseOut() {
-  const info = document.querySelector(".info"); // corrigido para classe
+  const info = document.querySelector(".info");
   info.style.display = "none";
 }
