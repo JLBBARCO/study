d3eb78cb429e5091f9c5058924e04946a4788746
Jose Luiz Bruiani Barco <jose.barco@escola.pr.gov.br>
2025-09-19 10:58:28 -0300

    19/09/2025.3

--- colegio_adolpho/3_serie/ciencias_dados/atividades/atividade08/ex08.py
+++ colegio_adolpho/3_serie/ciencias_dados/atividades/atividade08/ex08.py
@@ -1,4 +1,26 @@
+# * Importações
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+
+df = pd.read_csv('student_performance.csv')
+
+print(df.head())
+print(df.info())
+
+target_mapping = {
+    'F': 1,
+    'D': 2,
+    'C': 3,
+    'B': 4,
+    'A': 5
+}
+
+df['grade'] = df['grade'].map(target_mapping)
+
+corr = df.corr()
+
+plt.figure(figsize=(10, 8))
+sns.heatmap(corr, annot=True, cmap='coolwarm')
+plt.show()
