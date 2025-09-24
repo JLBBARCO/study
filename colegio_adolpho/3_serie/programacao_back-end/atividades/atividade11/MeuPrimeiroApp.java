6ee2075e3533d95f579fa30ad1b30e1852f8677d
Jose Luiz Bruiani Barco <jose.barco@escola.pr.gov.br>
2025-09-24 11:59:17 -0300

    24/09/2025

---
+++ colegio_adolpho/3_serie/programacao_back-end/atividades/atividade11/MeuPrimeiroApp.java
@@ -0,0 +1,16 @@
+public class MeuPrimeiroApp {
+    public static void main (String[] args) {
+        // String nome = "José Luiz";
+        // int idade = 17;
+        // float altura = 1.71f;
+        // System.out.println("Olá, " + nome + "! Você tem " + idade + " anos e " + altura + " de altura");
+        // System.out.print("Seja ");
+        // System.out.println("bem "); // ln após o print significa que a linha será quebrada após ser mostrado a informação desse print
+        // System.out.print("vindo!");
+        int x = 3;
+        if (x == 3) {
+            System.out.println("O x deve ser 3");
+        }
+        System.out.println("Isso vai ser exibido.");
+    }
+}
