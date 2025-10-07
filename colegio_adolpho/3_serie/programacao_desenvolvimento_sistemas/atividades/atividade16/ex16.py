a9e4c7c2412805a6a92023b925a88ba7c96c27ba
Jose Luiz Bruiani Barco <jose.barco@escola.pr.gov.br>
2025-10-07 08:50:18 -0300

    07/10/2025

---
+++ colegio_adolpho/3_serie/programacao_desenvolvimento_sistemas/atividades/atividade16/ex16.py
@@ -0,0 +1,23 @@
+'''
+Desenvolvido por José Luiz Bruiani Barco
+'''
+
+# * Importações
+import mysql.connector
+
+# * Parâmetros de conexão
+conexão = mysql.connector.connect(
+    host='127.0.0.1',
+    user='root',
+    password='',
+    database='exemplo'
+)
+
+# * Criar cursor
+meuCursor = conexão.cursor()
+
+# * Verificar conexão
+if conexão.is_connected():
+    print('Conexão bem sucedida!')
+
+meuCursor.execute('CREATE TABLE pessoas (nome VARCHAR(60), idade INT)')
