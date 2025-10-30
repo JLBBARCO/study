e9661721562b706e3a48f311e5ac7e18beb6bfd5
JLBBARCO <jbruianibarco@gmail.com>
2025-10-30 14:56:12 -0300

    30/10/2025.3

--- libs.bat
+++ libs.bat
@@ -1,6 +1,6 @@
 pip install customtkinter
 pip install mysql-connector-python
-npm install --global expo-cli
+npm install expo-cli
 npx empo install react-dom react-native-web
 python -m pip install pandas
 python -m pip install matplotlib
