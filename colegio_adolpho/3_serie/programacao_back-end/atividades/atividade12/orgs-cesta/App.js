054b9dcad6d68852631981bdc69ce1e064c20cad
Jose Luiz Bruiani Barco <jose.barco@escola.pr.gov.br>
2025-10-30 12:40:34 -0300

    30/10/2025.2

---
+++ colegio_adolpho/3_serie/programacao_back-end/atividades/atividade12/orgs-cesta/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>ERROR 404: NOT FOUND</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  texto: {
+    fontSize: 75,
+    fontWeight: "bold",
+    color: "red",
+  },
+});
