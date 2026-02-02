6351820939974330de939e13fb6b7ca2f55407ef
Jose Luiz Bruiani Barco <159829462+JLBBARCO@users.noreply.github.com>
2026-02-02 17:35:35 -0300

    2.2.26.17.35

---
+++ hashtag_programacao/react/audible/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import brasCubasIMG from "./assets/bras_cubas.jpeg";
+
+function App() {
+  const informacoesLivro = {
+    nome: "Memórias Póstumas de Brás Cubas",
+    autor: "Machado de Assis",
+    totalCapitulos: 2,
+    capa: brasCubasIMG,
+    textoAlternativo: "Capa do livro Memórias Póstumas de Brás Cubas",
+  };
+
+  return <></>;
+}
+
+export default App;
