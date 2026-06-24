6ff8761c7991d85c4e803b640ec4196ed70bdb60
Jose Luiz Bruiani Barco <159829462+JLBBARCO@users.noreply.github.com>
2026-06-24 09:32:55 -0300

    Add Adds

--- src/js/adsense.js
+++ src/js/adsense.js
@@ -1,12 +1,14 @@
-function adSense(scriptSrc, metaName) {
-  var adScript = document.createElement("script");
-  adScript.async = true;
-  adScript.src = scriptSrc;
-  adScript.setAttribute("crossorigin", "anonymous");
-  document.head.appendChild(adScript);
+const scriptSrc =
+  "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3137007272676866";
+const metaName = "";
 
-  var adMeta = document.createElement("meta");
-  adMeta.name = "google-adsense-account";
-  adMeta.content = metaName;
-  document.head.appendChild(adMeta);
-}
+var adScript = document.createElement("script");
+adScript.async = true;
+adScript.src = scriptSrc;
+adScript.setAttribute("crossorigin", "anonymous");
+document.head.appendChild(adScript);
+
+var adMeta = document.createElement("meta");
+adMeta.name = "google-adsense-account";
+adMeta.content = metaName;
+document.head.appendChild(adMeta);
