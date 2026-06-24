5fd04c990370f0b2cdced5873db8e492f5906163
Jose Luiz Bruiani Barco <159829462+JLBBARCO@users.noreply.github.com>
2026-06-24 09:44:54 -0300

    Update AdSense

--- src/js/adsense.js
+++ src/js/adsense.js
@@ -1,14 +1,17 @@
-const scriptSrc =
-  "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3137007272676866";
-const metaName = "";
+const publisherId = "ca-pub-3137007272676866";
+const scriptSrc = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${publisherId}`;
 
+// Criando e adicionando o script do AdSense ao <head>
 var adScript = document.createElement("script");
 adScript.async = true;
 adScript.src = scriptSrc;
 adScript.setAttribute("crossorigin", "anonymous");
 document.head.appendChild(adScript);
 
-var adMeta = document.createElement("meta");
-adMeta.name = "google-adsense-account";
-adMeta.content = metaName;
-document.head.appendChild(adMeta);
+// Criando e adicionando a meta tag de verificação da conta ao <head>
+if (publisherId && publisherId !== "") {
+  var adMeta = document.createElement("meta");
+  adMeta.name = "google-adsense-account";
+  adMeta.content = publisherId;
+  document.head.appendChild(adMeta);
+}
